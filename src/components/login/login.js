a08c69f7b9c637c9550533bf77b571ea647ee1be
Francesco Mariani <francesco.mariani@flowing.it>
2022-08-23 16:27:09 +0200

    AWS Amplify initialized

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,11 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import './login.scss';
+import config from '../../aws-exports';
+import { Auth, Hub } from 'aws-amplify';
+
+Auth.configure(config);
 
 export class Login extends React.Component {
 
+   constructor(props){
+    super(props);
+   }
+
     login(){
+
         
+
+        Auth.federatedSignIn({ provider: "Google", });
+   
     }
 
     render(){
